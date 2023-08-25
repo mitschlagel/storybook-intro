a76331bd21aac5bdddef0253b88fdfd86a165f47
Spencer Jones <spencerjones@fnni.com>
2023-08-25 10:34:26 -0500

    Add first component

---
+++ components/Task.stories.jsx
@@ -0,0 +1,30 @@
+// Task.stories.jsx
+
+import { Task } from "./Task";
+
+export default {
+    title: "Title",
+    component: Task,
+    argTypes: {
+        onPinTask: { action: "onPinTask" },
+        onArchiveTask: { action: "onArchiveTask" },
+    },
+};
+
+export const Default = {
+    args: {
+        task: {
+            id: "1",
+            title: "Test Task",
+            state: "TASK_INBOX"
+        },
+    },
+};
+
+export const Pinned = {
+    args: { task: { ...Default.args.task, state: "TASK_PINNED" } },
+};
+
+export const Archived = {
+    args: { task: { ...Default.args.task, state: "TASK_ARCHIVED" } },
+};
